f34584062810f632065abde86898bf95125f99cf
Lam Nguyen <nguyentamhoanglam@gmail.com>
2025-07-17 00:14:13 +0000

    update readme; add agent abstract
    Signed-off-by: Lam Nguyen <nguyentamhoanglam@gmail.com>

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # kromaplus
 
 ## Setup
+
+1. Spin up a RunPod instance
+
+2. Create conda env
 ```
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 conda create --name kromaplus python=3.12 -y
+source ~/.bashrc # or ~/.zshrc if you're using zsh
 conda init
 conda activate kromaplus
 ```
--- agent/agent.py
+++ agent/agent.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
 import logging
-from typing import Union
+from typing import Any, Awaitable, List, AsyncGenerator, Union
 
 from langchain_together import ChatTogether
+from langchain_core.language_models.base import BaseLanguageModel
+from langchain_openai.chat_models.base import BaseChatOpenAI
+from langchain_core.tools import BaseTool
 
 # Setup logging
 logging.basicConfig(level=logging.INFO)
@@ -15,7 +18,18 @@ class AgentMeta(ABC):
     @abstractmethod
     def __init__(
         self,
-        llm: Union[ChatTogether]
+        llm: Union[ChatTogether, BaseLanguageModel, BaseChatOpenAI],
+        tools: List[Union[str, BaseTool]]
     ):
         """initialize a new agent with LLM and tools"""
+        pass
+
+    @abstractmethod
+    def invoke(self, query: str, *args, **kwargs) -> Any:
+        """synchronously invoke agent's main function"""
+        pass
+
+    @abstractmethod
+    async def ainvoke(self, query: str, *args, **kwargs) -> Awaitable[Any]:
+        """asynchronously invoke the agent's main function"""
         pass
